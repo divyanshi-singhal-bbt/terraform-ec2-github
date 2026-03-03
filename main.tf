0bd630f3d9fa4672dd9156309e851caff41ee5c3
divyanshi-singhal-bbt <divyanshi.singhal@bellblazetech.com>
2026-03-03 13:47:11 +0530

    Fix EC2 subnet issue and use default VPC

--- main.tf
+++ main.tf
@@ -4,9 +4,20 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets in the default VPC
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id     = data.aws_subnet_ids.default.ids[0]  # pick first subnet
 
   tags = {
     Name = "Manual-Terraform-EC2"
